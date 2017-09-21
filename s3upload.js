abaaefe6afe94e5093af5b37ea1ffbc614780494
hsootree <hsootree@gmail.com>
2017-09-21 15:17:33 +0900

    install sharp

--- s3upload.js
+++ s3upload.js
@@ -4,13 +4,13 @@ const s3 = new aws.S3({
   apiVersion: '2006-03-01'
 })
 
-const buffer = fs.readFileSync('producer.js') // 업로드 할 파일
+const buffer = fs.readFileSync('southgate_hs.jpg') // 업로드 할 파일
 
 s3.upload({
   ACL: 'public-read', // 익명의 사용자도 파일 경로만 알면 읽기 가능하도록 설정
   Body: buffer,
   Bucket: 'wpsn-s3-hsootree',
-  Key: 'producer.js',
+  Key: 'southgate_hs.jpg',
   ContentDisposition: 'inline', // Content-Disposition 헤더
   ContentType: 'text/javascript' // Content-Type 헤더
 }, (err, data) => {
