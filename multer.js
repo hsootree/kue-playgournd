d6bafa0d20ea775c8757a5a84d6184cc762c2ab5
hsootree <hsootree@gmail.com>
2017-09-21 15:58:12 +0900

    add multer

---
+++ multer.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const multer = require('multer')
+
+const app = express()
+const upload = multer()
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+
+app.post('/', upload.single('photo'), (req, res) => {
+  sharp(req, file, buffer)
+    .resize(200, 200)
+    .crop(sharp.gravity.center)
+    .toFile('output2.png', (err, info) => {
+      consol.e.log(info)
+      res.redirect('/') // 변환이 끝난 후 리다이렉트
+    })
+})
+
+app.listen(3000, () => {
+  console.log('listening.... 3000')
+})
